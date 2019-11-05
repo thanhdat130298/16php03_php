f5f1d0f5473cca12cb6f29f423d214c32fe798a5
thanhdat130298 <nguyenthanhdat1302@gmail.com>
2019-11-05 16:42:54 +0700

    aaa

---
+++ SESS3/testdatabase.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRODUCT</title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<meta name="keywords" content="PHP, mysql">
+	<meta name="decription" content="This is register form">
+
+</head>
+<body>
+	<?php
+
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "product";
+	$port = "8080";
+
+	// Create connection
+	$connect = new mysqli($servername, $username, $password, $db);
+	// Check connection
+	if ($connect->connect_error) {
+	    die("Connection failed: " . $connect->connect_error);
+	}
+		$name = $decription = $price = $product = $dateUp = $dateOut = $file = '';
+		$errName = $errPrice = $errDec = $errFile = $errProduct = $errDateUp = $errDate = $errDateOut = '';
+			$arrProduct = array('stone' => 'Cục đá', 'paper' => 'Miếng giấy', 'wood' => 'Khúc gỗ');
+
+		$check = true;
+		if(isset($_POST['submit'])){
+			$name = $_POST['name'];
+			$price = $_POST['price'];
+			$product = $_POST['product'];
+			$decription = $_POST['decription'];
+			$dateUp = $_POST['dateUp'];
+			$dateOut = $_POST['dateOut'];
+			$fileName = '';
+
+			if($name == ''){
+				$errName = 'Nhập tên sản phẩm!';
+				$check = false;
+			}
+			if($price == ''){
+				$errPrice = 'Nhập giá sản phẩm!';
+				$check = false;
+			}
+			else if(!is_numeric($price)) {
+				$errPrice = 'Phải là só!';
+				$check = false;
+			}
+			if($product == ''){
+				$errProduct = 'Nhập tên sản phẩm!';
+				$check = false;
+				}
+			if($decription == ''){
+				$errDec = 'Nhập mô tả sản phẩm!';
+				$check = false;
+			}
+			if($dateUp == ''){
+				$errDateUp = 'Nhập ngày đăng sản phẩm!';
+				$check = false;
+			}
+			if($dateOut == ''){
+				$errDateOut = 'Nhập hết hạn sản phẩm!';
+				$check = false;
+			}
+			if($dateOut<$dateUp) {
+				$errDate = 'Nhap sai ngay!';
+				$check = false;
+
+			}
+			$upload = $_FILES['img'];
+			if(isset($upload)){
+				if ($upload['error'] > 0){
+					$errFile = "File lỗi!";
+					$check=false;
+				}
+				else if ($upload['error']==0){
+					$fileName = $upload['name'];
+					move_uploaded_file($upload['tmp_name'], 'uploads/'.$fileName);
+				}
+			}
+			else $errFile = "Bạn chưa chọn file!";
+			if($check){
+				echo "<div>";
+				echo "Tên sản phẩm : $name<br>";
+				echo "Mô tả: $decription<br>";
+				echo "Giá: $price<br>";
+				echo "<img src='uploads/$fileName'><br>";
+				echo "Ngày đăng: $dateUp<br>";
+				echo "Ngày hết hạn: $dateOut<br>";
+				echo "Sản phẩm: $arrProduct[$product]<br>";	
+				echo "</div>";
+				$sql = "INSERT INTO products (NAME, DECRIPTION, COST, IMG, DAYUP, DATEOUT, CATEGORY) VALUES ('$name', '$decription', '$price', '$fileName', '$dateUp', '$dateOut', '$product')";
+				if ($connect->query($sql) === TRUE) {
+			        echo "<span> Thêm dữ liệu thành công</span>";
+			    } else {
+			        echo "Error: " . $sql . "<br>" . $connect->error;
+			    }
+
+			}
+		}
+		
+
+	$connect->close();
+
+
+	?>
+	<form method="POST" enctype="multipart/form-data">
+		<label>Tên sản phẩm: </label><br>
+		<input type="text" name="name" class="input" value="<?php echo $name; ?>"><br>
+		<span><?php echo $errName;?></span>	<br>
+
+		<label>Mô tả: </label><br>
+		<textarea placeholder="Nhập mô tả..." name="decription"><?php echo $decription?></textarea><br>
+		<span><?php echo $errDec;?></span>	<br>
+
+		<label>Giá: </label><br>
+		<input type="text" class="input" name="price" value="<?php echo $price; ?>"><br>
+		<span><?php echo $errPrice;?></span>	<br>
+
+		<label>Hình ảnh:</label><br>
+		<input type="file" name="img"  ><br>
+		<span><?php echo $errFile; ?></span><br>
+
+		<label>Ngày đăng:  </label><br>
+		<input type="date" class="input" name="dateUp" value="<?php echo $dateUp; ?>"><br>
+		<span><?php echo $errDateUp;?></span>	<br>
+
+		<label>Ngày hết hạn:  </label><br>
+		<input type="date" class="input" name="dateOut" value="<?php echo $dateOut; ?>"><br>
+		<span><?php echo $errDateOut;?></span>	<br>
+		<span><?php echo $errDate ?></span><br>
+		<label>Sản Phẩm: </label><br>
+		<select name="product">
+			<option value="">Danh mục sản phẩm---</option>
+			<option value="stone" <?php echo $product == 'stone'?'selected':'' ?>>Cục đá</option>
+			<option value="wood" <?php echo $product == 'wood'?'selected':'' ?>>Khúc gỗ</option>
+			<option value="paper" <?php echo $product == 'paper'?'selected':'' ?>>Miếng giấy</option>
+		</select><br>
+		<span><?php echo $errProduct?></span><br>
+		<input class="but" type="submit" name="submit" value="SUBMIT">
+	</form>
+</body>
+</html>
--- sess2/form2.php
+++ sess2/form2.php
@@ -1,48 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>REGISTER</title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="icon_24h.ico">
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<meta name="keywords" content="PHP, mysql">
+	<meta name="decription" content="This is register form">
 
-<style type="text/css">
-	form {
-		border: 1px solid red;
-		width: 500px;
-		margin: 0 auto;
-		text-align: center;
+</head>
+<body>
+	
 
-	}
-	label {
-		font-size: 30px;
-	}
-	span {
-		color: red;
-		font-size: 18px;
-	}
-	input {
-		outline: none;
-		font-size: 20px;
-	}
-	select {
-		width: 400px;
-		height: 50px;
-		font-size: 25px;
-	}
-	option {
-		font-size: 25px;
-	}
-	.input {
-		text-align: center;
-		width: 400px;
-		height: 50px;
-	}
-	.but {
-		margin-top: 20px;
-		margin-bottom: 20px;
-		background-color: #8d82d6;
-		color: white;
-		border: none;
-		width: 100px;
-		height: 50px;
-	}
-</style>
 <?php
-	$name=$email=$Mchecked=$Fchecked='';  
+	$name = $email = $Mchecked = $Fchecked = $select =  '';  
 
 	$errName = $errEmail = $errDate = $errGender = $errSelect = '';
 	$check = true;
@@ -111,35 +82,51 @@
 	}
 
 ?>
-<form method="POST">
-		<label>Họ và tên: </label><br>
-		<input type="text" name="name" class="input" value="<?php echo $name; ?>"><br>
-		<span><?php echo $errName;?></span>	<br>
-		
-		<label>Giới tính :</label><br>
-		<input type="radio" name="gender" value="Male" <?php if($gender="Male") echo $Mchecked  ?>>
-		<label class="radio">Male</label>	<br>
-		<input type="radio" name="gender" value="Female"  <?php if($gender="Female") echo $Fchecked  ?>>
-		<label class="radio">Female</label><br>	<br>
-		<span><?php echo $errGender;?></span>	<br>
+	<form method="POST">
+			<label>Họ và tên: </label><br>
+			<input type="text" name="name" class="input" value="<?php echo $name; ?>"><br>
+			<span><?php echo $errName;?></span>	<br>
+			
+			<label>Giới tính :</label><br>
+			<input type="radio" name="gender" value="Male" <?php if($gender="Male") echo "checked"  ?>>
+			<label class="radio">Male</label>	<br>
+			<input type="radio" name="gender" value="Female"  <?php if($gender="Female") echo "checked"  ?>>
+			<label class="radio">Female</label><br>	<br>
+			<span><?php echo $errGender;?></span>	<br>
 
-		<label>Địa chỉ: </label><br>
-	<select name="select"  ><br>
-		<option value="null">--Chọn đi em...</option>
+			<label>Địa chỉ: </label><br>
+			<label class="a">Tỉnh/TP:</label><select name="select" >
+			<option value="null">--Chọn Tỉnh...</option>
+			<?php
+				include"connect.php";
+				$sql = "SELECT * FROM address";
+				$query = $conn->prepare($sql);
+				$query->execute();
+				$result = $query->fetchAll(PDO::FETCH_ASSOC);
+				foreach ($resule as $row ) {
+					echo '<option value="'.$ro">--Chọn Tỉnh...</option>
+'
 
-   		<option value="Đà Nẵng"<?php if($select=="Đà Nẵng") echo "selected"?>>Đà Nẵng</option>
-   		<option value="Quảng Nam"<?php if($select=="Quảng Nam") echo " selected"?>>Quảng Nam</option>
-	</select><br>
-	<span><?php echo $errSelect;?></span>	<br>
+					# code...
+				}
+		</select><br>
+		<span><?php echo $errSelect;?></span>	<br>
+		<label class="a">Quận/Huyện:	</label>
+		<select name="district" id="">
+			<option value="" class=""></option>
+		</select><br>
 
-	<label>Email: </label><br>
-	<input type="text" class="input" name="email" value="<?php echo $email; ?>"><br>
-	<span><?php echo $errEmail;?></span>	<br>
+		<label>Email: </label><br>
+		<input type="text" class="input" name="email" value="<?php echo $email; ?>"><br>
+		<span><?php echo $errEmail;?></span>	<br>
 
-	<label>Ngày sinh: </label><br>
-	<input type="date" class="input" name="date" data-date-format="DD MMMM YYYY" value="<?php echo $date; ?>"><br>
-	<span><?php echo $errDate;?></span>	<br>
+		<label>Ngày sinh: </label><br>
+		<input type="date" class="input" name="date" data-date-format="DD MMMM YYYY" value="<?php echo $date; ?>"><br>
+		<span><?php echo $errDate;?></span>	<br>
 
-	<input class="but" type="submit" name="submit" value="SUBMIT">
-	<input class="but" type="reset" name="reset" value="Reset" >
-</form>
+		<input class="but" type="submit" name="submit" value="SUBMIT">
+		<input class="but" type="reset" name="reset" value="Reset" >
+	</form>
+	<script src="js.js"></script>
+</body>
+</html>
